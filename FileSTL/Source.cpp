1d5e53a0b7c8a0bd51186b2e372c9558c3ba866c
TinyTinni <m_moeller@live.de>
2016-11-18 14:30:30 +0100

    cleanup stuff

--- FileSTL/Source.cpp
+++ FileSTL/Source.cpp
@@ -3,7 +3,7 @@
 #include <array>
 #include <fstream>
 
-
+#include <type_traits>
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
@@ -13,40 +13,60 @@
 #include <boost/spirit/include/phoenix_object.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 
-struct vec3
+////////////////////////////////////////////////////////////////////////////////
+// vector
+template<typename T>
+struct basic_vec3
 {
-    float x, y, z;
+    using scalar_type = T;
+    static_assert(std::is_floating_point<scalar_type>::value, "T must be a floating point");
+    T data[3];
 };
 
-typedef std::array<vec3, 3> vertices_t;
+BOOST_FUSION_ADAPT_TPL_STRUCT(
+    (T),
+    (basic_vec3)(T),
+    (T, data[0])
+    (T, data[1])
+    (T, data[2])
+)
 
-struct solid
+using dvec3 = basic_vec3<double>;
+using vec3 = basic_vec3<float>;
+
+//
+
+////////////////////////////////////////////////////////////////////////////////
+// solid type
+template<typename T>
+struct basic_solid
 {
+    using vec3 = basic_vec3<T>;
+    using scalar_type = T;
     std::string header; //solid name or header
     std::vector<vec3> normals;
     std::vector<vec3> vertices;
     std::vector<std::uint16_t> attributes; //only available in binary files
 };
+
 //
-BOOST_FUSION_ADAPT_STRUCT(
-    vec3,
-    (float, x)
-    (float, y)
-    (float, z)
-)
-//
-BOOST_FUSION_ADAPT_STRUCT(
-    solid,
-    (std::string, header)
-    (std::vector<vec3>, normals)
-    (std::vector<vec3>, vertices)
+BOOST_FUSION_ADAPT_TPL_STRUCT(
+    (T),
+    (basic_solid) (T),
+    (std::string, header),
+    (std::vector< basic_vec3<T> >, normals),
+    (std::vector< basic_vec3<T> >, vertices),
     (std::vector<std::uint16_t>, attributes)
 )
 
 
+using dsolid = basic_solid<double>;
+using solid = basic_solid<float>;
 
-template <typename Iterator>
-struct stl_ascii_parser : boost::spirit::qi::grammar<Iterator, solid(), boost::spirit::ascii::space_type>
+////////////////////////////////////////////////////////////////////////////////
+// Parsers
+template <typename T, typename Iterator>
+struct stl_ascii_parser : boost::spirit::qi::grammar<Iterator, basic_solid<T>(), boost::spirit::ascii::space_type>
 {
    
     stl_ascii_parser() : stl_ascii_parser::base_type(start)
@@ -99,15 +119,15 @@ struct stl_ascii_parser : boost::spirit::qi::grammar<Iterator, solid(), boost::s
                 );
     }
 
-    boost::spirit::qi::rule<Iterator, solid(), boost::spirit::ascii::space_type> start;
+    boost::spirit::qi::rule<Iterator, basic_solid<T>(), boost::spirit::ascii::space_type> start;
     boost::spirit::qi::rule<Iterator, std::string(), boost::spirit::ascii::space_type> startSolid;
-    boost::spirit::qi::rule<Iterator, vec3(), boost::spirit::ascii::space_type> vector;
-    boost::spirit::qi::rule<Iterator, std::vector<vec3>(), boost::spirit::ascii::space_type> vertices;
+    boost::spirit::qi::rule<Iterator, basic_vec3<T>(), boost::spirit::ascii::space_type> vector;
+    boost::spirit::qi::rule<Iterator, std::vector<basic_vec3<T>>(), boost::spirit::ascii::space_type> vertices;
     boost::spirit::qi::rule<Iterator, void(std::string), boost::spirit::ascii::space_type> endSolid;
 };
 
-template <typename Iterator>
-struct stl_binary_parser : boost::spirit::qi::grammar<Iterator, solid()>
+template <typename T, typename Iterator>
+struct stl_binary_parser : boost::spirit::qi::grammar<Iterator, basic_solid<T>()>
 {
 
     stl_binary_parser() : stl_binary_parser::base_type(start)
@@ -133,23 +153,79 @@ struct stl_binary_parser : boost::spirit::qi::grammar<Iterator, solid()>
             ;
     }
 
-    boost::spirit::qi::rule<Iterator, solid()> start;
-    boost::spirit::qi::rule<Iterator, vec3()> vector;
+    boost::spirit::qi::rule<Iterator, basic_solid<T>()> start;
+    boost::spirit::qi::rule<Iterator, basic_vec3<T>()> vector;
     boost::spirit::qi::rule<Iterator, void(std::string)> endSolid;
 };
 
-template <typename Iterator>
-struct stl_parser : boost::spirit::qi::grammar<Iterator, solid(), boost::spirit::ascii::space_type>
+template <typename T, typename Iterator>
+struct stl_parser : boost::spirit::qi::grammar<Iterator, basic_solid<T>(), boost::spirit::ascii::space_type>
 {
     stl_parser() : stl_parser::base_type(start)
     {
         start %= stl_ascii_ | boost::spirit::qi::lexeme[stl_binary_];
     }
-    stl_ascii_parser<Iterator> stl_ascii_;
-    stl_binary_parser<Iterator> stl_binary_;
+    stl_ascii_parser<T, Iterator> stl_ascii_;
+    stl_binary_parser<T, Iterator> stl_binary_;
     boost::spirit::qi::rule<Iterator, solid(), boost::spirit::ascii::space_type> start;
 };
 
+////////////////////////////////////////////////////////////////////////////////
+// Interface functions
+
+template<typename T, typename IterT>
+inline bool read_stl(basic_solid<T>& out, IterT begin, IterT end)
+{
+    stl_parser< T, IterT > p;
+    return boost::spirit::qi::phrase_parse(begin, end, p, boost::spirit::ascii::space, out);
+}
+
+template<typename T, typename IterT>
+inline std::tuple< basic_solid<T>, bool > read_stl(IterT begin, IterT end)
+{
+    basic_solid<T> o;
+    bool r = read_stl(o, begin, end);
+    return std::make_tuple<basic_solid<T>, bool>(std::move(o), std::move(r));
+}
+
+template<typename IterT>
+inline std::tuple< solid, bool > read_stl(IterT begin, IterT end)
+{
+    return read_stl<float>(begin, end);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+inline bool read_stl(basic_solid<T>& out, const std::string& fileName)
+{
+    std::ifstream f(fileName, std::ios::binary);
+    std::string str;
+    f.seekg(0, std::ios::end);
+    str.resize((unsigned)f.tellg());
+    f.seekg(0, std::ios::beg);
+    f.read(&str[0], str.size());
+    f.close();
+
+    return read_stl<T>(out, str.cbegin(),str.cend());
+}
+
+template<typename T>
+inline std::tuple<basic_solid<T>, bool> read_stl(const std::string& fileName)
+{
+    basic_solid<T> out;
+    bool r = read_stl(out, fileName);
+    return std::make_tuple<basic_solid<T>,bool>(std::move(out), std::move(r));
+}
+
+inline std::tuple<solid, bool> read_stl(const std::string& fileName)
+{
+    return read_stl<float>(fileName);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// stupid main
 
 int main()
 {
@@ -163,20 +239,10 @@ int main()
         endfacet\
         endsolid Hi";
 
-    stl_parser<std::string::const_iterator > b;
+    stl_parser<float , std::string::const_iterator > b;
     solid output, output2;
     bool r1 = boost::spirit::qi::phrase_parse(parsing.begin(), parsing.end(), b, boost::spirit::ascii::space, output);
-
-    std::ifstream f("C:\\Users\\Matthias\\Desktop\\ship.stl", std::ios::binary);
-    std::string str;
-    f.seekg(0, std::ios::end);
-    str.resize(f.tellg());
-    f.seekg(0, std::ios::beg);
-    f.read(&str[0], str.size());
-    f.close();
-
     
-    
-    bool r2 = boost::spirit::qi::phrase_parse(str.begin(),str.end(), b, boost::spirit::ascii::space, output2);
+//    bool r2 = boost::spirit::qi::phrase_parse(str.begin(),str.end(), b, boost::spirit::ascii::space, output2);
     system("Pause");
 }
